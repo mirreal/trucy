055df861cc890c56ff61bcf47abf34f59bb28305
mirreal <zgkm181142521@gmail.com>
2018-02-08 15:52:17 +0800

    add downlaod project template

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    env: {
+        node: true,
+        commonjs: true,
+        es6: true
+    },
+    globals: {
+    },
+    extends: [
+        'link-base'
+    ],
+    rules: {
+        'no-console': 0
+    }
+}
---
+++ bin/trucy.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+const program = require('commander')
+const pkg = require('../package.json')
+const { downloadTemplate } = require('../lib/')
+
+program
+    .version(pkg.version)
+    .usage('[options] <project-name>')
+    .arguments('<project-name>')
+    .action(name => {
+        downloadTemplate(name)
+    })
+
+program.parse(process.argv)
+
+if (!program.args.length) {
+    program.help()
+}
